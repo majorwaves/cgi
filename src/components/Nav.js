df555a638ab67cd37d5dcd1d3b9507f8291e2507
Jermaine Davis <jermaine@Jermaines-MacBook-Pro.local>
2019-03-15 09:37:08 -0700

    Created subnav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,114 +1,185 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import Search from './Search'
 import { NavLink } from 'react-router-dom'
-import SubNav from './SubNav';
+import Modal from 'react-modal'
+import ContactForm from './ContactForm'
+import SubNav from './SubNav'
+import navMarker from '../images/nav-marker.svg';
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)',
+    zIndex                : '10',
+    cursor                : 'auto'
+  }
+};
 
 const Menu = styled.nav`
   color: ${props => props.theme.color.medium};
   box-sizing: border-box;
   display: flex;
-  align-self: flex-end;
-  /* display: grid;
-  grid-template-columns: 1fr 1fr; */
+  align-items: center;
+  height: calc(48px + 2rem);
 
   .nav {
     padding: 0;
     margin: 0;
+    height: 100%;
     list-style: none;
     display: flex;
-    align-items: flex-end;
+    align-items: center;
     justify-content: space-between;
     position: relative;
     width: 40vw;
-    min-width: 900px;
+    min-width: 700px;
+  }
+`;
 
-    li {
-      text-transform: uppercase;
-      font-family: ${props => props.theme.type.sans};
-      margin-right: 4rem;
-      font-weight: 500;
-      font-size: 14px;
-      padding-bottom: 2.5rem;
+const Item = styled.li`
+  text-transform: uppercase;
+  font-family: ${props => props.theme.type.sans};
+  font-weight: 600;
+  font-size: 14px;
+  height: 100%;
+  display: flex;
+  align-items: center;
 
-      &:hover {
-        .subnav {
-          display: block;
-        }
+
+  &.has-subnav {
+    cursor: pointer;
+
+    &:hover {
+      background: url(${navMarker}) no-repeat;
+      background-position: center bottom;
+
+      .subnav {
+        display: block;
       }
+    }
+  }
 
-      &.link {
-        cursor: pointer;
+  &.link {
+    cursor: pointer;
 
-        &:hover {
-          color: ${props => props.theme.color.light};
-        }
-      }
+    &:hover {
+      color: ${props => props.theme.color.light};
+    }
+  }
 
-      a {
-        color: ${props => props.theme.color.medium};
-        text-decoration: none;
+  a {
+    color: ${props => props.theme.color.medium};
+    text-decoration: none;
 
-        &.active {
-          position: relative;
+    &.active {
+      position: relative;
 
-          &:after {
-            position: absolute;
-            bottom: -40px;
-            content: "";
-            height: 6px;
-            width: 100%;
-            background: ${props => props.theme.color.lightGrey};
-            display: block;
-          }
-        }
-
-        &:hover {
-          color: ${props => props.theme.color.light};
-        }
-      }
-
-      &:last-of-type {
-        margin-right: 0;
+      &:after {
+        position: absolute;
+        bottom: -40px;
+        content: "";
+        height: 6px;
+        width: 100%;
+        background: ${props => props.theme.color.lightGrey};
+        display: block;
       }
     }
 
-    &.main {
-      justify-content: flex-start;
-
-      li {
-        font-weight: 800;
-        font-size: 16px;
-
-        &.active {
-          position: relative;
-
-          &:after {
-            bottom: -20px;
-          }
-        }
-      }
+    &:hover {
+      color: ${props => props.theme.color.light};
     }
+  }
+
+  &:last-of-type {
+    margin-right: 0;
   }
 `;
 
 class Nav extends Component {
 
+  state = {
+    modalOpen: false
+  }
+
+  handleOpenModal = () => {
+    this.setState({ modalOpen: !this.state.modalOpen })
+  }
+
   render() {
+
+    const firmNav = [
+      {
+        title: 'Overview',
+        url: '/the-firm',
+        text: "Right here we'll put a blurb about the firm",
+      },
+      {
+        title: 'Leadership',
+        url: '/leadership',
+        text: "Right here we'll put a blurb about our team"
+      }
+    ]
+
+    const platformNav = [
+      {
+        title: 'Fund I',
+        url: '/fund',
+        text: "Right here we'll put the blurb about Fund I",
+        items:[
+          {
+            title: 'Overview',
+            url: '/fund/overview'
+          },
+          {
+            title: 'Strategy',
+            url: '/fund/strategy'
+          },
+          {
+            title: 'Portfolio',
+            url: '/fund/portfolio'
+          }
+        ]
+      },
+      {
+        title: 'SMA',
+        url: '/sma',
+        text: "Right here we'll put the blurb about SMA",
+        items: [
+          {
+            title: 'Overview',
+            url: '/sma/overview'
+          },
+          {
+            title: 'Assets',
+            url: '/sma/assets'
+          },
+        ]
+      }
+    ]
 
     return (
       <Menu>
         <ul className='nav main'>
-          <li><NavLink to='/the-firm'>The Firm</NavLink></li>
-          <li>
+          <Item className='has-subnav'>
+            The Firm
+            <SubNav items={firmNav} />
+          </Item>
+          <Item className='has-subnav'>
             Platforms
-            <SubNav />
-          </li>
-          <li><NavLink to='/team'>Team</NavLink></li>
-          <li><NavLink to='/market'>Market</NavLink></li>
-          <li><NavLink to='/social-impact'>Social Impact</NavLink></li>
-          <li><NavLink to='/media'>Media</NavLink></li>
+            <SubNav items={platformNav} />
+          </Item>
+          <Item><NavLink to='/market'>Market</NavLink></Item>
+          <Item><NavLink to='/social-impact'>Social Impact</NavLink></Item>
+          <Item><NavLink to='/media'>Media</NavLink></Item>
+          <Item className='link' onClick={this.handleOpenModal}>Contact</Item>
         </ul>
+        <Modal isOpen={this.state.modalOpen} onRequestClose={this.handleOpenModal} contentLabel='Contact' style={customStyles} overlayClassName='Modal_Overlay'>
+          <ContactForm />
+        </Modal>
       </Menu>
     );
   }
--- src/components/SubNav.js
+++ src/components/SubNav.js
@@ -6,60 +6,51 @@ const Wrapper = styled.div`
   position: absolute;
   z-index: 10;
   display: none;
-  top: 59px;
-  width: 100%;
-  transform: translate(-180px, 0);
+  top: calc(48px + 2rem);
+  left: 0;
+  transform: translate(-100px, 0);
+  width: 40vw;
+  min-width: 900px;
   box-sizing: border-box;
-`;
-
-const Caret = styled.div`
-  position: absolute;
-  display: block;
-  width: 0;
-  height: 0;
-  border-width: 10px;
-  border-style: solid;
-  border-color: transparent transparent ${props => props.theme.color.dark};
-  top: -20px;
-  left: 200px;
-  z-index: 1;
 `;
 
 const Box = styled.div`
   background: ${props => props.theme.color.dark};
   color: white;
   display: grid;
-  grid-template-columns: 120px 300px;
-  padding: 1rem;
+  grid-template-columns: 500px 300px;
+  padding: 1rem 0;
+  height: 500px;
+
 `;
 
 const List = styled.ul`
   margin: 0;
   list-style: none;
   display: block;
-  border-right: 1px solid rgba(255,255,255,0.4);
-  padding: 1rem;
+  border-right: 1px solid rgba(255,255,255,0.2);
   line-height: 2rem;
+  padding: 0;
+  width: 250px;
+`;
 
-  li {
-    font-size: 14px;
-    margin: 0 !important;
-    padding: 0 !important;
+const ListItem = styled.li`
+  font-size: 14px;
+  margin: 0 !important;
+  padding: 0 !important;
 
-    a {
-      color: white !important;
-
-      &:hover {
-        opacity: 0.6;
-      }
-    }
+  a.main {
+    color: white !important;
+    display: block;
+    padding: .25rem 1rem;
+    background: ${props => props.active ? props.theme.color.medium : 'transparent'}
   }
 `;
 
 const Info = styled.div`
   padding: 1rem 1rem 1rem 3rem;
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   justify-content: flex-end;
   font-weight: normal !important;
   font-size: 18px !important;
@@ -67,21 +58,82 @@ const Info = styled.div`
   text-transform: none !important;
 `;
 
+const SubList = styled.ul`
+  position: absolute;
+  left: 251px;
+  top: 0;
+  width: 250px;
+  margin: 1rem 0 0 0;
+  grid-area: sub;
+  line-height: 2rem;
+  box-sizing: border-box;
+  border-right: 1px solid rgba(255,255,255,0.1);
+  list-style: none;
+  padding: 0;
+  height: calc(100% - 2rem);
+`;
+
+const SubListItem = styled.li`
+  font-size: 14px;
+  margin: 0 !important;
+  padding: 0 !important;
+
+  a {
+    color: white !important;
+    display: block;
+    padding: .25rem 1rem;
+
+    &:hover {
+      background: ${props => props.theme.color.medium};
+    }
+  }
+`;
+
 class SubNav extends Component {
+
+  state = {
+    current: 0
+  }
+
+  handleHover = (index) => {
+    this.setState({ current: index })
+  }
+
 
   render() {
     return (
       <Wrapper className='subnav'>
-        <Caret/>
-        <Box>
-          <List>
-            <li><Link to='/fund'>Fund I</Link></li>
-            <li><Link to='/sma'>SMA</Link></li>
-          </List>
-          <Info>
-            <span>Right here we'll put a blurb about the fund</span>
-          </Info>
-        </Box>
+      <Box>
+        <List>
+          {this.props.items.map((item, index) => {
+            return (
+              <ListItem key={index} active={index === this.state.current}>
+                <Link className='main' onMouseEnter={() => { this.handleHover(index) }} to={item.url}>
+                  {item.title}
+                  </Link>
+                  {item.items
+                    ?
+                      <SubList>
+                       {this.props.items[this.state.current].items.map((subItem, index) => {
+                         return (
+                           <SubListItem key={index}>
+                             <Link  to={subItem.url}>
+                              {subItem.title}
+                             </Link>
+                           </SubListItem>
+                         )
+                       })}
+                      </SubList>
+                    : null
+                  }
+              </ListItem>
+            )}
+          )}
+        </List>
+        <Info>
+          {this.props.items[this.state.current].text}
+        </Info>
+      </Box>
       </Wrapper>
     );
   }
