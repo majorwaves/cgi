31190094fa8619f7cdac1cda2d897e564194b5af
Jermaine Davis <jermaine@Jermaines-MacBook-Pro.local>
2019-03-15 09:36:21 -0700

    Style Button to match specs

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
+import { device } from '../utils/devices'
 
 const Wrapper = styled.button`
-  padding: 1rem 2rem;
-  border-radius: 30px;
-  font-size: 16px;
+  width: 100%;
+  padding: .5rem 1rem;
+  font-size: 12px;
   font-weight: 500;
   font-family: ${props => props.theme.type.body};
   text-transform: uppercase;
-  letter-spacing: 0.2rem;
-  background: ${props => props.primary ? props.theme.color.dark : 'transparent' };
+  letter-spacing: 0.1rem;
+  background: ${props => props.primary ? 'transparent' : props.theme.color.dark };
   color: ${props => props.primary ? 'white' : props.theme.color.light };
-  border: ${props => props.primary ? `2px solid ${props.theme.color.dark}` : `2px solid ${props.theme.color.light}` };
+  border: ${props => props.primary ? `1px solid white` : `1px solid ${props.theme.color.light}` };
   cursor: pointer;
   transition: 0.2s all ease-in-out;
+  text-decoration: none;
+
+  @media ${device.laptop}{
+    display: inline-flex;
+    width: fit-content;
+  }
 
   &:hover {
     transform: translate(0, -2px);
