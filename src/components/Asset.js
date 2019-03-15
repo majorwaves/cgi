e0e1d156c54b789bbe5d8ac79eccfd01ff2009fd
Jermaine Davis <jermaine@Jermaines-MacBook-Pro.local>
2019-03-15 09:36:13 -0700

    Style Asset to match specs

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -3,12 +3,9 @@ import styled from 'styled-components'
 import Button from './Button'
 import { Link } from 'react-router-dom'
 
-const Block = styled.div`
-
-`;
 
 const Image = styled.div`
-  height: 30vw;
+  height: 500px;
   overflow: hidden;
   transition: 0.2s all ease-in-out;
 
@@ -16,38 +13,67 @@ const Image = styled.div`
     width: 100%;
     height: 100%;
     object-fit: cover;
+    transition: 0.2s all ease-in-out;
+  }
+`;
+
+const Block = styled.div`
+  position: relative;
+
+  &:hover {
+    ${Image} {
+      img {
+        transform: scale(1.02);
+      }
+    }
   }
 `;
 
 const Info = styled.div`
   background: white;
-  padding: 1rem 2rem 2rem;
-  text-align: center;
+  padding: 2rem 3rem;
 
-  h3 {
-    color: ${props => props.theme.color.lightGrey};
-    font-weight: 500;
-    letter-spacing: 0.3rem;
-    text-transform: uppercase;
-    font-size: 16px;
-    font-family: ${props => props.theme.type.body};
+  p {
+    margin-bottom: 2rem;
+    color: ${props => props.theme.color.grey};
+    line-height: 1.6em;
   }
+
+  button {
+    border-color: ${props => props.theme.color.medium};
+    color: ${props => props.theme.color.medium};
+  }
+`;
+
+const Title = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 500px;
+  pointer-events: none;
+  box-sizing: border-box;
+  text-align: center;
+  z-index: 1;
 
   h2 {
     font-family: ${props => props.theme.type.heading};
     font-weight: normal;
-    font-size: 36px;
-    color: ${props => props.theme.color.dark};
+    font-size: 60px;
+    color: white;
+    text-transform: uppercase;
   }
 `;
 
-const Asset = ({title, city, image, url}) => (
+const Asset = ({title, description, image, url}) => (
   <Block>
-    <Image><img src={image} alt={title} /></Image>
+    <Title><h2>{title}</h2></Title>
+    <Link to={url}>
+      <Image><img src={image} alt={title} /></Image>
+    </Link>
     <Info>
-      <h3>{city}</h3>
-      <h2>{title}</h2>
-      <Link to={url}><Button>Learn More</Button></Link>
+      <p>{description}</p>
+      <Link to={url}><Button primary>Learn More</Button></Link>
     </Info>
   </Block>
 );
