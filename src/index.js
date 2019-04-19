2cae65bdcfbe18ca8756a5463ac2ad9736871938
Jermaine Davis <jermaine@Jermaines-MacBook-Pro.local>
2019-04-18 21:31:42 -0700

    Removed colons

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { BrowserRouter as Router } from 'react-router-dom'
 import ScrollToTop from './utils/ScrollToTop'
 
 ReactDOM.render(
