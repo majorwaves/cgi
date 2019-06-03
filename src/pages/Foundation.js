b1e6a3df7ed22e93e1a775058248ded77cd65173
Jermaine Davis <jermaine@Jermaines-MacBook-Pro.local>
2019-06-02 20:06:14 -0700

    created foundation page

---
+++ src/pages/Foundation.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import PageHeaderSlider from '../components/PageHeaderSlider'
+import image from '../images/foundation-1.jpg'
+import image2 from '../images/foundation-2.jpg'
+
+const Wrapper = styled.div`
+
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 90vw;
+  max-width: 1000px;
+  margin: 6rem auto;
+  grid-gap: 4rem;
+
+  div {
+    img {
+      width: 100%;
+    }
+
+    h4 {
+      color: #6EA558;
+      font-size: 12px;
+      margin: 0.5rem 0 0;
+    }
+
+    p {
+      font-size: 20px;
+      margin-top: 0.5rem;
+      color: ${props => props.theme.color.dark};
+      font-family: ${props => props.theme.type.heading};
+    }
+  }
+`;
+
+
+class Foundation extends Component {
+
+  render() {
+    return (
+      <Wrapper>
+        <PageHeaderSlider
+          image={image}
+          secondImage={image2}
+          title='By Isabela Alycia'
+          text='The story of the Isabela Alycia Foundation is about empowering students and mothers'
+        />
+        <Grid>
+          <div>
+            <img src='https://sites.clinton-foundation.org/wp-content/uploads/2019/05/cgi-thumb-square-600x600.jpg' />
+            <h4>CREATING ECONOMIC OPPORTUNITY</h4>
+            <p>Etiam dictum nibh quam, sit amet hendrerit felis facilisis eu</p>
+          </div>
+          <div>
+            <img src='https://sites.clinton-foundation.org/wp-content/uploads/2019/05/cdi-thumb-square-600x600.jpg' />
+            <h4>CREATING ECONOMIC OPPORTUNITY</h4>
+            <p>Etiam dictum nibh quam, sit amet hendrerit felis facilisis eu</p>
+          </div>
+          <div>
+            <img src='https://sites.clinton-foundation.org/wp-content/uploads/2019/05/chmi-thumb-square-600x600.jpg' />
+            <h4>CREATING ECONOMIC OPPORTUNITY</h4>
+            <p>Etiam dictum nibh quam, sit amet hendrerit felis facilisis eu</p>
+          </div>
+          <div>
+            <img src='https://sites.clinton-foundation.org/wp-content/uploads/2019/05/2PM8725-1-600x600.jpg' />
+            <h4>CREATING ECONOMIC OPPORTUNITY</h4>
+            <p>Etiam dictum nibh quam, sit amet hendrerit felis facilisis eu</p>
+          </div>
+          <div>
+            <img src='https://sites.clinton-foundation.org/wp-content/uploads/2019/05/cci-thumb-square-600x600.jpg' />
+            <h4>CREATING ECONOMIC OPPORTUNITY</h4>
+            <p>Etiam dictum nibh quam, sit amet hendrerit felis facilisis eu</p>
+          </div>
+          <div>
+            <img src='https://sites.clinton-foundation.org/wp-content/uploads/2019/05/ahg-thumb-square-600x600.jpg' />
+            <h4>CREATING ECONOMIC OPPORTUNITY</h4>
+            <p>Etiam dictum nibh quam, sit amet hendrerit felis facilisis eu</p>
+          </div>
+        </Grid>
+      </Wrapper>
+    );
+  }
+
+}
+
+export default Foundation;
--- src/pages/SocialImpact.js
+++ src/pages/SocialImpact.js
@@ -5,16 +5,20 @@ import school from '../images/school.jpg'
 import ReactPlayer from 'react-player'
 import Modal from 'react-modal'
 import { FaPlayCircle } from 'react-icons/fa'
-
+import { device } from '../utils/devices'
+import Helmet from 'react-helmet'
+import { Link } from 'react-router-dom'
+import foundation from '../images/foundation.jpg'
+import foundationLogo from '../images/alycia.svg'
 
 const customStyles = {
   content : {
     top                   : '50%',
-    left                  : '50%',
+    left                  : 'calc(10vw - 1rem)',
     right                 : 'auto',
     bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
+    width                 : '80vw',
+    transform             : 'translate(0, -50%)'
   }
 };
 
@@ -23,11 +27,53 @@ const Wrapper = styled.div`
 `;
 
 const Section = styled.section`
-  width: 80vw;
   margin: 4rem auto;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 5vw;
+  width: 90vw;
+
+  @media ${device.laptop}{
+    width: 80vw;
+    display: -ms-grid;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    -ms-grid-columns: 1fr 1fr;
+    grid-gap: 5vw;
+  }
+
+  div {
+    &:nth-of-type(2n){
+      -ms-grid-column: 2;
+      margin-left: 5vw;
+
+      @supports (display: grid){
+        margin-left: 0;
+      }
+    }
+  }
+
+  .callout {
+    position: relative;
+
+    a {
+      color: white;
+      text-decoration: none;
+    }
+
+    img {
+      display: inline-block;
+      width: 100%;
+      height: 400px;
+      object-fit: cover;
+    }
+
+    .alycia {
+      transform: translate(2rem, -10rem);
+      z-index: 2;
+      position: relative;
+      height: auto;
+      width: calc(100% - 4rem);
+      margin: 0 auto;
+    }
+  }
 
   .video {
     position: relative;
@@ -75,6 +121,57 @@ const Section = styled.section`
   img {
     width: 100%;
   }
+
+  h4 {
+    font-family: ${props => props.theme.type.sans};
+    font-size: 14px;
+    text-transform: uppercase;
+    font-weight: normal;
+    letter-spacing: 0.2rem;
+    margin: 0 0 1rem;
+    opacity: 0.4;
+    text-align: center;
+
+    @media ${device.laptop}{
+      margin: 0;
+      text-align: left;
+    }
+  }
+`;
+
+const Callout = styled.div`
+  z-index: 2;
+  transform: translate(2rem, -8rem);
+  background: ${props => props.theme.color.dark};
+  color: white;
+  left: 2rem;
+  width: calc(100% - 4rem);
+  box-sizing: border-box;
+  border-top: 3px solid ${props => props.theme.color.light};
+
+  h2 {
+    font-family: ${props => props.theme.type.heading};
+    font-weight: normal;
+    font-size: 36px;
+    padding: 1rem 2rem 0;
+  }
+
+  p {
+    padding: 0 2rem 1rem;
+  }
+
+  button {
+    appearance: none;
+    width: 100%;
+    text-transform: uppercase;
+    font-size: 16px;
+    font-weight: bold;
+    font-family: ${props => props.theme.type.body};
+    background: ${props => props.theme.color.medium};
+    color: white;
+    padding: 1rem;
+    border: 0;
+  }
 `;
 
 class SocialImpact extends Component {
@@ -91,6 +188,13 @@ class SocialImpact extends Component {
   render(){
     return (
       <Wrapper>
+        <Helmet title='CGI | Social Impact'>
+          <meta property="og:title" content="CGI | Social Impact" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="http://cgiimg.com/social-impact" />
+          <meta property="og:image" content="https://img.youtube.com/vi/iJlqkIjLpGA/maxresdefault.jpg" />
+          <meta property="og:description" content="CGI is committed to improving the lives of mothers and students globally. Through the Isabella Alycia Foundation, CGI leverages its resources and capital to empower single mother households and disadvantaged students." />
+        </Helmet>
         <PageHeader
           image={school}
           title='Social Impact'
@@ -100,28 +204,38 @@ class SocialImpact extends Component {
           <div>
             <h1>Isabella Alycia Foundation</h1>
             <p><strong>The Alice Miller Story</strong></p>
-            <p>Alice Miller Thomas impacted countless lives with compassion, love and care. She was a brilliant businesswomen; using her platforms as a channel to provide resources and tools to both single moms and young men at risk. </p>
-            <p> Alice gave inspiration to single moms globally; equipping them with lessons and tools to function as constructive members of society. She also provided capital and human resources that led those same women to live lives of greater self-reliance and independence.  Additionally, Alice challenged young men at risk to seek education as a way out of their dogmatic circumstances. She encouraged these men to rise to the challenge by helping them positively impact the societies they lived in.</p>
+            <p>Alice Miller Thomas impacted countless lives with compassion, love and care. She was a brilliant businesswomen; using her platforms as a channel to provide resources and tools to both single moms and young men at risk.</p>
+            <p> Alice gave inspiration to single moms globally; equipping them with lessons and tools to function as constructive members of society. She also provided capital and human resources that led those same women to live lives of greater self-reliance and independence. Additionally, Alice challenged young men at risk to seek education as a way out of their dogmatic circumstances. She encouraged these men to rise to the challenge by helping them positively impact the societies they lived in.</p>
+            <p>CGI is committed to improving the lives of mothers and students globally. Through the Isabella Alycia Foundation, CGI leverages its resources and capital to empower single mother households and disadvantaged students.</p>
           </div>
-          <div className='video' onClick={this.handleClick}>
-            <div className='icon'><FaPlayCircle /></div>
-            <img src='https://img.youtube.com/vi/iJlqkIjLpGA/maxresdefault.jpg' alt='play video' />
+          <div className='callout'>
+          <Link to='/foundation'>
+            <img src={foundation} alt='woman' />
+            <img src={foundationLogo} class='alycia' alt='Isabella Alycia' />
+            <Callout>
+                <h2>The Story of Isabela Alycia</h2>
+                <p>Read about Isabela Alycia and her plans to fulfil her grandmother’s legacy</p>
+                <button>Read Now</button>
+            </Callout>
+            </Link>
           </div>
         </Section>
         <Section>
           <div>
-            <h1>Our Goal</h1>
-            <p>The IA Foundation is focused on providing underprivileged students and single mothers with the knowledge and resources needed to improve their world.  Through strategic partnerships, we develop holistic programs that generate economic growth opportunities, improve health, and cultivate wellness.</p>
+            <h4>Commited to Our</h4>
+            <h1>Alliance & Scholars Program</h1>
+            <p>In 2019, CGI announced the Isabela Alycia Foundation to focus on providing underprivileged students and single mothers with the knowledge and resources needed to improve their world. Through strategic partnerships, the Foundation develops holistic programs that generate economic growth opportunities, improve health, and cultivate wellness.</p>
           </div>
           <div>
-            <h1>Our Purpose</h1>
-            <p>Building on the work of the late Alice Thomas, The IA Foundation is designed to make a real difference in the lives for underprivileged students and single mothers.</p>
-            <p>By working directly and with strategic partners, our goal is to capitalize on our expert resources to get the best possible results.</p>
+            <h4>Commited to</h4>
+            <h1>Supporting Families</h1>
+            <p>Building on the work of the late Alice Thomas, The Isabela Alycia Foundation is designed to make a real difference in the lives for underprivileged students and single mothers. By working directly and with strategic partners, the goal of CGI is to capitalize on our expert resources to get the best results within the Alliance & Scholars Program.</p>
           </div>
         </Section>
         <Modal isOpen={this.state.modalOpen} onRequestClose={this.handleClick} style={customStyles}>
           <ReactPlayer
             playing
+            className='video_wrapper'
             controls
             url='https://youtu.be/iJlqkIjLpGA'
           />
